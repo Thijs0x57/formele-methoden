cc6f2a389d8af04502c872fa63f5ddaf5f811738
Thijs W <mawijnen2@student.avans.nl>
2019-05-07 15:57:17 +0200

    Added base classes

---
+++ Automata/Automata/Automaton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata
+{
+    /**
+     * <typeparam name="T" State type
+     * <typeparam name="U" Alphabet type
+     * 
+     */
+    abstract class Automaton<T, U>
+    {
+        protected Dictionary<T, Dictionary<U, T>> automaton = new Dictionary<T, Dictionary<U, T>>();
+        private List<U> alphabet;
+
+        public Automaton(List<U> alphabet)
+        {
+            this.alphabet = alphabet;
+        }
+
+        public abstract void addTransition(U input, T fromState, T toState);
+    }
+}
---
+++ Automata/Automata/DFA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata
+{
+    class DFA<T, U> : Automaton<T, U>
+    {
+        public DFA(List<U> alphabet) : base(alphabet)
+        {
+        }
+
+        public override void addTransition(U input, T fromState, T toState)
+        {
+            if (base.automaton.ContainsKey(fromState))
+            {
+                
+            }
+        }
+    }
+}
