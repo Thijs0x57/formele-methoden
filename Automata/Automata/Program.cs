de25186c2078656048f88d2c4f7759b9dfcfe3df
Thijs W <mawijnen2@student.avans.nl>
2019-05-29 11:12:07 +0200

    Added regex functions

--- Automata/Automata/Program.cs
+++ Automata/Automata/Program.cs
@@ -4,15 +4,31 @@ namespace Automata
 {
     class Program
     {
+        private static Regex expr1, expr2, expr3, expr4, expr5, a, b, all;
         static void Main(string[] args)
         {
+            TestDFA();
+            TestNDFA();
+            TestRegExp();
+            testLanguage();
+        }
+
+        public static void tester(String stringToTest, bool accepted)
+        {
+            Console.WriteLine(
+                "Testing: " + stringToTest + "\n" +
+                "result: " + accepted + "\n");
+        }
+
+        public static void TestDFA()
+        {
             /**
-             *  
              *  bevat een even aantal b’s of bevat een oneven aantal a’s
              *  bevat een even aantal b’s en eindigt op aab
              *  begint met abb en bevat baab
              */
             // Begint met abb of eindigt op baab
+            Console.WriteLine("--------------DFA--------------");
             Automaton<int, char> dfa = new DFA<int, char>();
             dfa.addStartState(0);
             dfa.addEndState(3);
@@ -48,7 +64,11 @@ namespace Automata
             tester(test, dfa.accept(test.ToCharArray()));
             test = "aaaaaaabbbbbb";
             tester(test, dfa.accept(test.ToCharArray()));
+        }
 
+        public static void TestNDFA()
+        {
+            Console.WriteLine("--------------NDFA--------------");
             Automaton<int, char> ndfa = new NDFA<int, char>('$');
             ndfa.addStartState(1);
             ndfa.addEndState(9);
@@ -68,17 +88,43 @@ namespace Automata
             ndfa.addTransition('$', 3, 9);
             ndfa.addTransition('$', 1, 9);
             // Ends with a, or bc
-            String ndfaTest = "acbe";
+            String ndfaTest = "acb";
             tester(ndfaTest, ndfa.accept(ndfaTest.ToCharArray()));
             ndfaTest = "bbabaabbbca";
             tester(ndfaTest, ndfa.accept(ndfaTest.ToCharArray()));
         }
 
-        public static void tester(String stringToTest, bool accepted)
+        public static void TestRegExp()
         {
-            Console.WriteLine(
-                "Testing: " + stringToTest + "\n" +
-                "result: " + accepted + "\n");
+            a = new Regex("a");
+            b = new Regex("b");
+
+            // expr1: "baa"
+            expr1 = new Regex("baa");
+            // expr2: "bb"
+            expr2 = new Regex("bb");
+            // expr3: "baa | baa"
+            expr3 = expr1.or(expr2);
+
+            // all: "(a|b)*"
+            all = (a.or(b)).star();
+
+            // expr4: "(baa | baa)+"
+            expr4 = expr3.plus();
+            // expr5: "(baa | baa)+ (a|b)*"
+            expr5 = expr4.dot(all);
+        }
+
+        public static void testLanguage()
+        {
+            Console.WriteLine("--------------Regex--------------");
+            Console.WriteLine("taal van (baa):\n" + string.Join(" ", expr1.getLanguage(5)));
+            Console.WriteLine("taal van (bb):\n" + string.Join(" ", expr2.getLanguage(5)));
+            Console.WriteLine("taal van (baa | bb):\n" + string.Join(" ", expr3.getLanguage(5)));
+            
+            Console.WriteLine("taal van (a|b)*:\n" + string.Join(" ", all.getLanguage(5)));
+            Console.WriteLine("taal van (baa | bb)+:\n" + string.Join(" ", expr4.getLanguage(5)));
+            Console.WriteLine("taal van (baa | bb)+ (a|b)*:\n" + string.Join(" ", expr5.getLanguage(6)));
         }
     }
 }
--- Automata/Automata/Regex.cs
+++ Automata/Automata/Regex.cs
@@ -8,5 +8,141 @@ namespace Automata
 {
     class Regex
     {
+        private enum Operator
+        {
+            PLUS, STAR, OR, DOT, ONE
+        }
+        private Operator op;
+        private String terminals;
+        private Regex left;
+        private Regex right;
+
+        public class CompareByLength : IComparer<string>
+        {
+            public int Compare(string s1, string s2)
+            {
+                if (s1.Length == s2.Length)
+                { return s1.CompareTo(s2); }
+                else
+                { return s1.Length - s2.Length; }
+            }
+        }
+
+        public Regex()
+        {
+            op = Operator.ONE;
+            terminals = "";
+            left = null;
+            right = null;
+        }
+
+        public Regex(String p)
+        {
+            op = Operator.ONE;
+            terminals = p;
+            left = null;
+            right = null;
+        }
+
+        public Regex plus()
+        {
+            Regex result = new Regex();
+            result.op = Operator.PLUS;
+            result.left = this;
+            return result;
+        }
+
+        public Regex star()
+        {
+            Regex result = new Regex();
+            result.op = Operator.STAR;
+            result.left = this;
+            return result;
+        }
+
+        public Regex or(Regex e2)
+        {
+            Regex result = new Regex();
+            result.op = Operator.OR;
+            result.left = this;
+            result.right = e2;
+            return result;
+        }
+
+        public Regex dot(Regex e2)
+        {
+            Regex result = new Regex();
+            result.op = Operator.DOT;
+            result.left = this;
+            result.right = e2;
+            return result;
+        }
+
+        public SortedSet<String> getLanguage(int maxSteps)
+        {
+            SortedSet<String> emptyLanguage = new SortedSet<String>(new CompareByLength());
+            SortedSet<String> languageResult = new SortedSet<String>(new CompareByLength());
+
+            SortedSet<String> languageLeft, languageRight;
+
+            if (maxSteps < 1) return emptyLanguage;
+
+            switch (this.op)
+            {
+                case Operator.ONE:
+                    { languageResult.Add(terminals); }
+                    break;
+
+                case Operator.OR:
+                    languageLeft = left == null ? emptyLanguage : left.getLanguage(maxSteps - 1);
+                    languageRight = right == null ? emptyLanguage : right.getLanguage(maxSteps - 1);
+                    languageResult.UnionWith(languageLeft);
+                    languageResult.UnionWith(languageRight);
+                    break;
+
+
+                case Operator.DOT:
+                    languageLeft = left == null ? emptyLanguage : left.getLanguage(maxSteps - 1);
+                    languageRight = right == null ? emptyLanguage : right.getLanguage(maxSteps - 1);
+                    foreach (String s1 in languageLeft)
+                        foreach (String s2 in languageRight)
+                        { languageResult.Add(s1 + s2); }
+                    break;
+
+                // STAR(*) en PLUS(+) kunnen we bijna op dezelfde manier uitwerken:
+                case Operator.STAR:
+                case Operator.PLUS:
+                    languageLeft = left == null ? emptyLanguage : left.getLanguage(maxSteps - 1);
+                    languageResult.UnionWith(languageLeft);
+                    for (int i = 1; i < maxSteps; i++)
+                    {
+                        HashSet<String> languageTemp = new HashSet<String>(languageResult);
+                        foreach (String s1 in languageLeft)
+                        {
+                            foreach (String s2 in languageTemp)
+                            {
+                                languageResult.Add(s1 + s2);
+                            }
+                        }
+                    }
+                    if (this.op == Operator.STAR)
+                    { languageResult.Add(""); }
+                    break;
+
+
+                default:
+                    Console.WriteLine("getLanguage is nog niet gedefinieerd voor de operator: " + this.op);
+                    break;
+            }
+
+
+            return languageResult;
+        }
+
+
+        public NDFA<int, char> toNDFA()
+        {
+            return null;
+        }
     }
 }
